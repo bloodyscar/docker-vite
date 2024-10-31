418945078e3b4f7b8f1a03e1c842cff3b416fa8d
Adit <adithernowo@gmail.com>
2024-10-31 13:23:02 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './index.css';
+import Card from './components/Card';
+
+
+function App() {
+  return (<div>
+    <Card />
+    <Card />
+  </div>)
+
+
+}
+
+export default App
